97e7727ed1545510c1e04b119188401d07e4f6af
Chuturica <chuturica@gmail.com>
2022-01-27 11:42:31 +0100

    Named Slots in a Nutshell

--- main.js
+++ main.js
@@ -1,41 +1,9 @@
-// window.Event = new Vue();
 
-window.Event = new class{
-    constructor(){
-        this.vue = new Vue();
-
-    }
-
-    fire(event, data = null){
-        this.vue.$emit(event,data);
-    }
-
-    listen (event, callback){
-        this.vue.$on(event,callback);
-    }
-}
-
-
-Vue.component('coupon',{
-    template: '<input placeholder="Enter zour coupon code" @blur="onCouponApplied">',
-
-    methods: {
-        onCouponApplied(){
-            Event.fire('applied');
-        }
-    }
-});
 
 new Vue({
     el: '#root',
 
-    data: {
-        couponApplied: false
-    },
-
-    created(){
-        Event.listen('applied', ()=> alert('Handliga it !!!!'));
-    }
+    
 
 });
 
