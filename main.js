e3aaa29855819d4c03e084aa47402a1a3ef5e135
Chuturica <chuturica@gmail.com>
2022-01-27 11:39:45 +0100

    Component Communication Example#2:Event Dispatcher

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+// window.Event = new Vue();
+
+window.Event = new class{
+    constructor(){
+        this.vue = new Vue();
+
+    }
+
+    fire(event, data = null){
+        this.vue.$emit(event,data);
+    }
+
+    listen (event, callback){
+        this.vue.$on(event,callback);
+    }
+}
+
+
 Vue.component('coupon',{
     template: '<input placeholder="Enter zour coupon code" @blur="onCouponApplied">',
 
     methods: {
         onCouponApplied(){
-            this.$emit('applied' );
+            Event.fire('applied');
         }
     }
 });
@@ -15,10 +33,8 @@ new Vue({
         couponApplied: false
     },
 
-    methods: {
-        onCouponApplied(){
-            this.couponApplied= true;
-        }
+    created(){
+        Event.listen('applied', ()=> alert('Handliga it !!!!'));
     }
 
 });
