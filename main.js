c92a227e7ffea8c71e65d201fb17caa5c4d93a2e
Chuturica <chuturica@gmail.com>
2022-01-27 09:55:02 +0100

    start Ep 11

--- main.js
+++ main.js
@@ -1,33 +1,4 @@
-Vue.component('modal', {
-    props: ['title', 'body'],
 
-    data() {
-        return {
-            isVisible: true
-        };
-    },
-
-    template: `
-        
-        <div class="modal is-active">
-            <div class="modal-background"></div>
-                <div class="modal-content">
-                    <!-- Any other Bulma elements you want -->
-                    <div class="box">
-                        <slot></slot>
-                    </div>
-                    
-                </div>
-            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
-        </div>
-    `,
-
-    methods: {
-        hideModel() {
-            this.isVisible = false;
-        }
-    }
-});
 
 
 new Vue({
