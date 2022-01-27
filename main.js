55d3bb9bfec25f69f17765bd32d6312f24107b37
Chuturica <chuturica@gmail.com>
2022-01-27 11:19:13 +0100

    add metods

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
+Vue.component('coupon',{
+    template: '<input placeholder="Enter zour coupon code" @blur="onCouponApplied">',
+
+    methods: {
+        onCouponApplied(){
+            alert('applierd!');
+        }
+    }
+});
 
 new Vue({
     el: '#root',
 
-})
+    methods: {
+        onCouponApplied(){
+            alert('It vas applied!');
+        }
+    }
+
+});
+
