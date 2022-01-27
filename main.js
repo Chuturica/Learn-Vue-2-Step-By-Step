925986ab2b21fed484f4aeb8db5bd31e2e61e3a9
Chuturica <chuturica@gmail.com>
2022-01-27 11:48:11 +0100

    add Model card

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-
+Vue.component('modal',{
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Modal title</p>
+                    <button class="delete" aria-label="close"></button>
+                </header>
+                <section class="modal-card-body">
+                    Templorary content for now
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button is-success">Save changes</button>
+                    <button class="button">Cancel</button>
+                </footer>
+            </div>
+    </div>
+    `
+});
 
 new Vue({
     el: '#root',
